05b5f4e48572e58c38b47b9c525271fd2696e9c3
Leonid Radkevich <laradkevich@gmail.com>
2018-11-06 18:56:06 +0300

    Add project files.

---
+++ week3/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+using System;
+
+namespace week3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("commit me to github");
+            Console.ReadLine();
+        }
+    }
+}
