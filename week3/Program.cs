a597397ce9317aa0581d1f9395386edc720d47e4
Leonid Radkevich <laradkevich@gmail.com>
2018-11-06 19:00:35 +0300

    added for loop for console writeline

--- week3/Program.cs
+++ week3/Program.cs
@@ -9,6 +9,10 @@ namespace week3
         static void Main(string[] args)
         {
             Console.WriteLine("commit me to github");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("commit me to github");
+            }
             Console.ReadLine();
         }
     }
